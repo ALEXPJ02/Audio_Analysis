5f3b3baaef6ce2a9b0324ca7b3b4fb3c611adeda
Alexandro Jauregui <alexandro.jauregui1225@gmail.com>
2025-05-26 11:56:39 +1000

    phase 1 ✅ Goals: 	•	Set up the basic folder structure. 	•	Add required dependencies. 	•	Create config and logging system.

---
+++ utils/logger.py
@@ -0,0 +1,27 @@
+import logging
+import os
+from datetime import datetime
+
+def setup_logger(name: str, log_dir: str = "data/logs") -> logging.Logger:
+    os.makedirs(log_dir, exist_ok=True)
+    log_file = os.path.join(log_dir, f"{name}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log")
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # File handler
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(logging.DEBUG)
+
+    # Console handler
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    return logger
